8b2e7a29607c7c66d8396f5b1b44b9233888c8af
Tyler Stocks <56895599+TylerAStocks@users.noreply.github.com>
2024-01-02 14:20:46 -0500

    Add files via upload

---
+++ module/helpers/config.mjs
@@ -0,0 +1,54 @@
+export const PNP = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ PNP.abilities = {
+  "agi": "Agility",
+  "int": "Intellect",
+  "mig": "Might",
+  "per": "Perception",
+  "tou": "Toughness",
+  "wil": "Willpower"
+};
+
+PNP.abilityAbbreviations = {
+  "agi": "PNP.AbilityAgiAbbr",
+  "int": "PNP.AbilityIntAbbr",
+  "mig": "PNP.AbilityMigAbbr",
+  "per": "PNP.AbilityPerAbbr",
+  "tou": "PNP.AbilityTouAbbr",
+  "wil": "PNP.AbilityWilAbbr"
+};
+
+
+PNP.talents = {
+  "aca": "Academics",
+  "cha": "Charm",
+  "com": "Command",
+  "cov": "Covert",
+  "inv": "Investigation",
+  "med": "Medicine",
+  "pro": "Professional",
+  "sci": "Science",
+  "str": "Streetwise",
+  "sur": "Survival",
+  "tec": "Technology",
+  "veh": "Vehicles"
+}
+
+PNP.talentAbbreviations = {
+  "aca": "PNP.TalentAcaAbbr",
+  "cha": "PNP.TalentChaAbbr",
+  "com": "PNP.TalentComAbbr",
+  "cov": "PNP.TalentCovAbbr",
+  "inv": "PNP.TalentInvAbbr",
+  "med": "PNP.TalentMedAbbr",
+  "pro": "PNP.TalentProAbbr",
+  "sci": "PNP.TalentSciAbbr",
+  "str": "PNP.TalentStrAbbr",
+  "sur": "PNP.TalentSurAbbr",
+  "tec": "PNP.TalentTecAbbr",
+  "veh": "PNP.TalentVehAbbr"
+}
---
+++ module/pnp.mjs
@@ -0,0 +1,137 @@
+// Import document classes.
+import { PnpActor } from "./documents/actor.mjs";
+import { PnpItem } from "./documents/item.mjs";
+// Import sheet classes.
+import { PnpActorSheet } from "./sheets/actor-sheet.mjs";
+import { PnpItemSheet } from "./sheets/item-sheet.mjs";
+// Import helper/utility classes and constants.
+import { preloadHandlebarsTemplates } from "./helpers/templates.mjs";
+import { PNP } from "./helpers/config.mjs";
+
+/* -------------------------------------------- */
+/*  Init Hook                                   */
+/* -------------------------------------------- */
+
+Hooks.once('init', async function() {
+
+  // Add utility classes to the global game object so that they're more easily
+  // accessible in global contexts.
+  game.pnp = {
+    PnpActor,
+    PnpItem,
+    rollItemMacro
+  };
+
+  // Add custom constants for configuration.
+  CONFIG.PNP = PNP;
+
+  /**
+   * Set an initiative formula for the system
+   * @type {String}
+   */
+  CONFIG.Combat.initiative = {
+    formula: "@attributes.edge.value",
+    decimals: 2
+  };
+
+  // Define custom Document classes
+  CONFIG.Actor.documentClass = PnpActor;
+  CONFIG.Item.documentClass = PnpItem;
+
+  // Register sheet application classes
+  Actors.unregisterSheet("core", ActorSheet);
+  Actors.registerSheet("pnp", PnpActorSheet, { makeDefault: true });
+  Items.unregisterSheet("core", ItemSheet);
+  Items.registerSheet("pnp", PnpItemSheet, { makeDefault: true });
+
+  // Preload Handlebars templates.
+  return preloadHandlebarsTemplates();
+});
+
+/* -------------------------------------------- */
+/*  Handlebars Helpers                          */
+/* -------------------------------------------- */
+
+// If you need to add Handlebars helpers, here are a few useful examples:
+Handlebars.registerHelper('concat', function() {
+  var outStr = '';
+  for (var arg in arguments) {
+    if (typeof arguments[arg] != 'object') {
+      outStr += arguments[arg];
+    }
+  }
+  return outStr;
+});
+
+Handlebars.registerHelper('toLowerCase', function(str) {
+  return str.toLowerCase();
+});
+
+/* -------------------------------------------- */
+/*  Ready Hook                                  */
+/* -------------------------------------------- */
+
+Hooks.once("ready", async function() {
+  // Wait to register hotbar drop hook on ready so that modules could register earlier if they want to
+  Hooks.on("hotbarDrop", (bar, data, slot) => createItemMacro(data, slot));
+});
+
+/* -------------------------------------------- */
+/*  Hotbar Macros                               */
+/* -------------------------------------------- */
+
+/**
+ * Create a Macro from an Item drop.
+ * Get an existing item macro if one exists, otherwise create a new one.
+ * @param {Object} data     The dropped data
+ * @param {number} slot     The hotbar slot to use
+ * @returns {Promise}
+ */
+async function createItemMacro(data, slot) {
+  // First, determine if this is a valid owned item.
+  if (data.type !== "Item") return;
+  if (!data.uuid.includes('Actor.') && !data.uuid.includes('Token.')) {
+    return ui.notifications.warn("You can only create macro buttons for owned Items");
+  }
+  // If it is, retrieve it based on the uuid.
+  const item = await Item.fromDropData(data);
+
+  // Create the macro command using the uuid.
+  const command = `game.pnp.rollItemMacro("${data.uuid}");`;
+  let macro = game.macros.find(m => (m.name === item.name) && (m.command === command));
+  if (!macro) {
+    macro = await Macro.create({
+      name: item.name,
+      type: "script",
+      img: item.img,
+      command: command,
+      flags: { "pnp.itemMacro": true }
+    });
+  }
+  game.user.assignHotbarMacro(macro, slot);
+  return false;
+}
+
+/**
+ * Create a Macro from an Item drop.
+ * Get an existing item macro if one exists, otherwise create a new one.
+ * @param {string} itemUuid
+ */
+function rollItemMacro(itemUuid) {
+  // Reconstruct the drop data so that we can load the item.
+  const dropData = {
+    type: 'Item',
+    uuid: itemUuid
+  };
+  // Load the item from the uuid.
+  Item.fromDropData(dropData).then(item => {
+    // Determine if the item loaded and if it's an owned item.
+    if (!item || !item.parent) {
+      const itemName = item?.name ?? itemUuid;
+      return ui.notifications.warn(`Could not find item ${itemName}. You may need to delete and recreate this macro.`);
+    }
+
+    // Trigger the item roll
+    item.roll();
+  });
+}
